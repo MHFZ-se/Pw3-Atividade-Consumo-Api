082488144a6ac134957131f5d9c66ad948b8011b
HENRI MUNIZ FUDALI <henri.fudali@e239.local>
2026-09-09 11:15:34 -0300

    a

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #projeto do site de filmes e series filmax
-from flask import Flask
+from flask import Flask, render_template # é a ferramenta encarregada de renderizar paginas htnl
+from controllers import routes
 
 app = Flask(__name__, template_folder='views')
 
-app.run(debug=True)
-    
+
+routes.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+     
