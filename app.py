52cb22eb23645a1db68a6c8a5853cb8ed58ee20b
unknown <henrifudali@gmail.com>
2026-09-14 00:52:49 -0300

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #projeto do site de filmes e series filmax
 from flask import Flask, render_template # é a ferramenta encarregada de renderizar paginas htnl
 from controllers import routes
+from flask import request 
 
 app = Flask(__name__, template_folder='views')
 
--- controllers/routes.py
+++ controllers/routes.py
@@ -1,21 +1,34 @@
 from flask import render_template,redirect, url_for
 import urllib.request
 import json
+from flask import request
 
 def init_app(app):
     
     @app.route('/', methods=['GET', 'POST'])
     def home():
+        
+        return render_template('index.html')
+    
+    @app.route('/series')
+    def lista():
         urlAPI = 'https://api.tvmaze.com/shows'
         resposta = urllib.request.urlopen(urlAPI)
         dados = resposta.read()
         #print(dados)
         listaObras = json.loads(dados)
         #retorna a lista inteira com tudo
+        
 
     # ROTA DE LOGOUT
-        return render_template('index.html', listaObras = listaObras)
-    
+        return render_template('lista.html', listaObras = listaObras)
+
+
     @app.route('/infos')
     def infos():
         return render_template('informacoes.html')
+    
+
+
+
+    
